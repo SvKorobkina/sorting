65077426ecd3811f0920e18f4ce248cdb4280fe5
Коробкина Светлана Андреевна <120242807+SvKorobkina@users.noreply.github.com>
2023-01-17 22:10:18 +0700

    Add files via upload

---
+++ src/Gnom.java
@@ -0,0 +1,20 @@
+public class Gnom{
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        int i = 0;
+        while (i < array.length) {
+            if (i == 0 || array[i - 1].compareTo(array[i]) <= 0) {
+                i++;
+            } else {
+                swap(array, i, i - 1);
+                i--;
+            }
+        }
+    }
+
+    private static <T extends Comparable<T>> void swap(
+            T[] array, int a, int b) {
+        T temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
